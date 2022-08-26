f7f70290af55c766faa087eeb2d14eff869154ba
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-08-25 19:39:49 -0700

    randomChoice function

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const rockButton = document.getElementById("rock-button");
-rockButton.addEventListener("click", computerChoice());
+//const rockButton = document.getElementById("rock-button");
+//rockButton.addEventListener("click", computerChoice());
 
-const paperButton = document.getElementById("paper-button");
-paperButton.addEventListener("click", computerChoice());
+//const paperButton = document.getElementById("paper-button");
+//paperButton.addEventListener("click", computerChoice());
 
-const scissorsButton = document.getElementById("paper-button");
-scissorsButton.addEventListener("click", computerChoice());
+//const scissorsButton = document.getElementById("paper-button");
+//scissorsButton.addEventListener("click", computerChoice());
+
+const choices = ["rock", "paper", "scissors"];
+
+function randomChoice () {
+    const randomSelection = Math.floor(Math.random() * choices.length);
+    return choices[randomSelection]
+}
+
+console.log(randomChoice());
