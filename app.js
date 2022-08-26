187c0dbeb7bae5155acf78afdfc6e57094700f41
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-08-25 19:13:50 -0700

    add button event listeners

---
+++ app.js
@@ -0,0 +1,8 @@
+const rockButton = document.getElementById("rock-button");
+rockButton.addEventListener("click", computerChoice());
+
+const paperButton = document.getElementById("paper-button");
+paperButton.addEventListener("click", computerChoice());
+
+const scissorsButton = document.getElementById("paper-button");
+scissorsButton.addEventListener("click", computerChoice());
