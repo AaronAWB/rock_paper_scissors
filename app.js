cf2b147882703bc4b003e23dd23cb4544bfd0cff
Aaron Brinckerhoff <aaron.brinckerhoff@gmail.com>
2022-08-27 11:02:46 -0700

    add playGame function

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const rockButton = document.getElementById("rock-button");
 const paperButton = document.getElementById("paper-button");
 const scissorsButton = document.getElementById("scissors-button");
 
-rockButton.addEventListener ("click", playGame("rock-button"));
-paperButton.addEventListener ("click", playGame("paper-button"));
-scissorsButton.addEventListener ("clock", playGame("scissors-button"));
+rockButton.addEventListener ("click", playGame("rock"));
+paperButton.addEventListener ("click", playGame("paper"));
+scissorsButton.addEventListener ("clock", playGame("scissors"));
 
 const choices = ["rock", "paper", "scissors"];
 
@@ -16,7 +16,30 @@ function computerChoice () {
 }
 
 function playGame (playerChoice) {
-   
+   if (playerChoice === computerChoice()) return "tie";
+   switch (playerChoice) {
+        case "rock":
+            if (computerChoice() === "paper") {
+                return "lose";
+                break;
+            } else {
+                return "win";
+            }
+        case "paper":
+            if (computerChoice() === "scissors") {
+                return "lose";
+                break;
+            } else {
+                return "win";
+            }
+        case "scissors":
+            if (computerChoice() === "rock") {
+                return "lose";
+                break;
+            } else {
+                return "win";
+            }
+   }
 }
     
     
